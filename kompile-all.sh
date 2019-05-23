d5a58077d96ced5389ee123fb9fd90e73694cfbf
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-05-24 00:47:28 +0300

    kompile info

--- kompile-all.sh
+++ kompile-all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#also run `mvn install -Dcheckstyle.skip -DskipTests` in K Framework
+
 ./kompile-clear.sh
 
 go run $GOPATH/src/github.com/ElrondNetwork/elrond-vm/iele/original/node/kompile
