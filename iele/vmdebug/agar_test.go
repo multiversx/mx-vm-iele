029f6dc9852e58e859342624bc2f72b85d41ac52
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-05-30 15:08:42 +0300

    agar contract v1

---
+++ iele/vmdebug/agar_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	eptest "github.com/ElrondNetwork/elrond-vm/iele/test-util/endpointtest"
+)
+
+func TestAgar(t *testing.T) {
+	eptest.TestAllInDirectory(t,
+		elrondTestRoot,
+		"agar",
+		elrondIeleProvider{},
+		world)
+}
