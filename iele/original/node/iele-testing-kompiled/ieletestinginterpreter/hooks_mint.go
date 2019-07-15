91a5fa0cf652872255a07906224a567e0525dc8d
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-07-15 12:45:33 +0300

    deleted generated sources for easier merge

--- iele/original/node/iele-testing-kompiled/ieletestinginterpreter/hooks_mint.go
+++
@@ -1,8 +0,0 @@
-// File provided by the K Framework Go backend. Timestamp: 2019-06-24 20:00:34.418
-
-package ieletestinginterpreter
-
-type mintHooksType int
-
-const mintHooks mintHooksType = 0
-
--- iele/original/standalone/iele-testing-kompiled/ieletestinginterpreter/hooks_mint.go
+++
@@ -1,8 +0,0 @@
-// File provided by the K Framework Go backend. Timestamp: 2019-06-24 23:27:10.928
-
-package ieletestinginterpreter
-
-type mintHooksType int
-
-const mintHooks mintHooksType = 0
-
