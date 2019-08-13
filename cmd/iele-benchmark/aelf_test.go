49acea851aa5d51c66c16011de198c38cb7e48a1
andrei-marinica <andrei_m_marinica@yahoo.com>
2019-08-13 19:12:14 +0300

    benchmark update

--- cmd/iele-benchmark/aelf_test.go
+++ cmd/iele-benchmark/aelf_test.go
@@ -13,7 +13,7 @@ func BenchmarkLoopExpNop1M(b *testing.B) {
 		"testNop(int,int,uint)",
 		big.NewInt(0),
 		big.NewInt(5),
-		big.NewInt(10000))
+		big.NewInt(1000000))
 }
 
 // go test -bench BenchmarkLoopDivAdd10M
@@ -25,5 +25,5 @@ func BenchmarkLoopDivAdd10M(b *testing.B) {
 		big.NewInt(100),
 		big.NewInt(300),
 		big.NewInt(500),
-		big.NewInt(10000))
+		big.NewInt(100000))
 }
